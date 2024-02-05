6d0d835f588422f90b60371283ee45e36d999aa0
aaditya prabu <aadityaprabu@gmail.com>
2024-02-05 21:41:26 +0530

    Day 5 Done

--- Day5/Part1/Solution.py
+++ Day5/Part1/Solution.py
@@ -1,18 +1,49 @@
+from pprint import pprint
+import math
 class Solution:
     #private
     __input = ''
     __output = ''
     def __read_input(self):
-        with open('input.txt') as file:
+        with open('input.txt','r') as file:
             self.__input = file.read()
 
         
     def __write_output(self):
-        with open('output.txt') as file:
+        with open('output.txt','w') as file:
             file.write(self.__output)
         pass
     
+    def __map_range(self,from_value,range_map):
+        cur_value = from_value
+        for r in range_map:
+            r = r.split(' ')
+            dest = int(r[0])
+            src = int(r[1])
+            rl = int(r[2])
+            if src <= cur_value and cur_value <= src+rl-1:
+                cur_value = (dest - src) + cur_value
+                return cur_value
+            
+        return cur_value
+
     def __logic(self):
+        data = self.__input.split('\n\n')
+        seeds = data[0].split(':')[1] 
+        ranges = []
+        for i in range(1,len(data)):
+            ranges.append(data[i].split(':')[1].split('\n')[1::])
+
+        min_value = math.inf
+        can_map = False
+        for seed in seeds.split(' ')[1::]:
+            cur = int(seed)
+
+            for r in range(len(ranges)):    
+                cur = self.__map_range(cur,ranges[r])
+            min_value = min(cur,min_value)
+  
+        self.__output = str(min_value)
         pass
 
     #public
--- Day5/Part2/Solution.py
+++ Day5/Part2/Solution.py
@@ -1,20 +1,58 @@
+import math
 class Solution:
     #private
     __input = ''
     __output = ''
     def __read_input(self):
-        with open('input.txt') as file:
+        with open('input.txt','r') as file:
             self.__input = file.read()
 
         
     def __write_output(self):
-        with open('output.txt') as file:
+        with open('output.txt','w') as file:
             file.write(self.__output)
         pass
     
-    def __logic(self):
-        pass
+   
 
+    def __logic(self):
+        data = self.__input.split('\n\n')
+        range_maps = []
+        for i in range(1,len(data)):
+            range_maps.append( data[i].split(':')[1].split('\n')[1::] )
+        
+        seeds = data[0].split(':')[1] 
+        seeds = list(map(int,seeds.split(' ')[1::]))  
+        temp = []
+        for i in range(0,len(seeds),2):
+            temp.append((seeds[i],seeds[i]+seeds[i+1]))
+
+        seeds = temp
+
+        for each_range_map in range_maps:
+            ranges = []
+            for r in each_range_map:
+                ranges.append(list(map(int,r.split(' '))))
+
+            res = []
+            while len(seeds) > 0:
+                start ,end = seeds.pop()
+                for dest,src,rl in ranges:
+                    overlap_start = max(start,src)
+                    overlap_end = min(end,src+rl)
+                    if overlap_start < overlap_end:
+                        res.append((overlap_start - src + dest,overlap_end - src + dest))
+                        if overlap_start > start:
+                            seeds.append((start, overlap_start))
+                        if end > overlap_end:
+                            seeds.append((overlap_end, end))
+                        break
+                else:
+                    res.append((start,end))
+            seeds = res
+
+        self.__output = str(min(res)[0])
+        pass
     #public
     def run(self):
         self.__read_input()
