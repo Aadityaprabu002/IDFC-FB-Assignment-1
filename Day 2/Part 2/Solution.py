537279b9bb0bac7f9e1c70edd8de8bd8dea074b4
aaditya prabu <aadityaprabu@gmail.com>
2024-02-13 12:18:46 +0530

    Day 2 2nd  part done

--- Day 2/Part 2/Solution.py
+++ Day 2/Part 2/Solution.py
@@ -1,19 +1,42 @@
+from collections import defaultdict
+from pprint import pprint
 class Solution:
     #private
     __input = ''
     __output = ''
     def __read_input(self):
-        with open('input.txt') as file:
+        with open('input.txt','r') as file:
             self.__input = file.read()
 
         
     def __write_output(self):
-        with open('output.txt') as file:
+        with open('output.txt','w') as file:
             file.write(self.__output)
         pass
     
     def __logic(self):
-        pass
+        data = self.__input.split('\n')
+        games = []
+        for row in data:
+            game = row.split(':')[1].strip().split(';')
+            games.append(game)
+
+        tot = 0
+        for game in games:
+            max_map = defaultdict(int)
+            for turn in game:
+                color_pairs = turn.split(',')
+                for color_pair in color_pairs:
+                    pair = color_pair.strip().split(' ')
+                    freq = int(pair[0])
+                    color = pair[1]
+                    max_map[color] = max(max_map[color],freq)
+            pprint(max_map)
+            
+            tot +=  max_map['red'] * max_map['blue'] * max_map['green']  
+                        
+        self.__output = str(tot)
+
 
     #public
     def run(self):
